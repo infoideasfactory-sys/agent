514d379f0ecd360483beaf329d4a176a7344873d
infoideasfactory-sys <info.ideasfactory@gmail.com>
2026-08-21 21:18:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agent-v1';
+const CACHE = 'agent-v5';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
